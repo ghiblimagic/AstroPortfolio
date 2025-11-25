def2fb72320267811aa4ce01b4b8ef019fb0c471
Janet Spellman <janetspellman13@gmail.com>
2025-11-24 20:32:34 -0800

    bug for mobile: fixed mobile nav bar bug, needed client:load for it to show

--- src/components/navigation/MobileNavContainer.jsx
+++ src/components/navigation/MobileNavContainer.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import MobileNav from "./MobileNav";
 import { useStore } from "@nanostores/react";
 import { isNavOpen } from "../../store/navStore";
 
 export default function MobileNavContainer() {
   const $isNavOpen = useStore(isNavOpen);
+  console.log("isNavOpen", $isNavOpen);
   return <div className="md:hidden">{$isNavOpen ? <MobileNav /> : null}</div>;
 }
