07272fdc6ce1da656c79d10c0b58bb0bba835481
Janet Spellman <janetspellman13@gmail.com>
2025-12-06 03:50:50 -0800

    refactoring: converted mobile nav bar to plain js and astro to improve loading time

--- src/components/navigation/MobileNavContainer.jsx
+++
@@ -1,9 +0,0 @@
-import MobileNav from "./MobileNav";
-import { useStore } from "@nanostores/react";
-import { isNavOpen } from "../../store/navStore";
-
-export default function MobileNavContainer() {
-  const $isNavOpen = useStore(isNavOpen);
-
-  return <div className="md:hidden">{$isNavOpen ? <MobileNav /> : null}</div>;
-}
